8cd308b672a199d135d1d08c1a77a20835fbc6af
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-08-22 11:29:12 -0300

    fineshed

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Rodrigo Rafael on 22/08/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Rodrigo Rafael on 22/08/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
