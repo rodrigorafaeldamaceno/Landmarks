d051310b0baf9706c202b8765a4fe45442622ec2
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-08-22 11:29:15 -0300

    fneshed

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,15 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Turtle Rock").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-        }.padding()
+            MapView().frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock").font(.title2)
+                Text("Descriptive text goes here")
+            }.padding()
+            
+            Spacer()
+        }
     }
 }
 
