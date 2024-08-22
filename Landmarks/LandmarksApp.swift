10c9350012b0c98064f7cfaa0f0d8aa5351097c0
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-08-22 10:56:09 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rodrigo Rafael on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
